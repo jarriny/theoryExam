236d41850afefd98db72ed0e6fa3967009f473c6
jarriny <71301995+jarriny@users.noreply.github.com>
2021-10-16 10:12:33 -0400

    Create xml_parse.py

---
+++ xml_parse.py
@@ -0,0 +1,25 @@
+import xml.etree.ElementTree as ET
+
+# https://docs.python.org/3/library/xml.etree.elementtree.html
+
+# parses the given xml file that gives an automata
+
+def main():
+    try:
+        file = input()
+    except EOFError:
+        text = ""
+    automata = ET.parse(file)
+    root = automata.getroot()
+    for state in root.iter('state'):
+        print(state.get('name') + " ", end='')
+    print()
+    for state in root.iter('state'):
+        if state.find('initial') != None:
+            print(state.get('name') + " ")
+    for state in root.iter('state'):
+        if state.find('final') != None:
+            print(state.get('name') + " ")
+
+if __name__ == "__main__":
+    main()
